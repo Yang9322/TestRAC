83a57ad88a7610441f560f2997b552d66d1c1f9b
heyang <heyang@kesucorp.com>
2017-12-18 14:05:39 +0800

    Initial Commit

---
+++ TestRAC/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TestRAC
+//
+//  Created by 贺杨 on 2017/12/18.
+//  Copyright © 2017年 贺杨. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ TestRAC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestRAC
+//
+//  Created by 贺杨 on 2017/12/18.
+//  Copyright © 2017年 贺杨. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
